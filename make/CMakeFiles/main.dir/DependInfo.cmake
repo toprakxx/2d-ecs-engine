6825a6f5a4f19e8ca37f53581a1a6570e214f67b
Enes Akkuşçi <enes.akkusci@sabanciuniv.edu>
2025-07-14 18:24:45 +0300

    First build

---
+++ make/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Administrator/Projects/Cpp/2d_ecs_engine/libs/header/glm/detail/glm.cpp" "CMakeFiles/main.dir/libs/header/glm/detail/glm.cpp.obj" "gcc" "CMakeFiles/main.dir/libs/header/glm/detail/glm.cpp.obj.d"
+  "C:/Users/Administrator/Projects/Cpp/2d_ecs_engine/libs/header/imgui/imgui.cpp" "CMakeFiles/main.dir/libs/header/imgui/imgui.cpp.obj" "gcc" "CMakeFiles/main.dir/libs/header/imgui/imgui.cpp.obj.d"
+  "C:/Users/Administrator/Projects/Cpp/2d_ecs_engine/libs/header/imgui/imgui_demo.cpp" "CMakeFiles/main.dir/libs/header/imgui/imgui_demo.cpp.obj" "gcc" "CMakeFiles/main.dir/libs/header/imgui/imgui_demo.cpp.obj.d"
+  "C:/Users/Administrator/Projects/Cpp/2d_ecs_engine/libs/header/imgui/imgui_draw.cpp" "CMakeFiles/main.dir/libs/header/imgui/imgui_draw.cpp.obj" "gcc" "CMakeFiles/main.dir/libs/header/imgui/imgui_draw.cpp.obj.d"
+  "C:/Users/Administrator/Projects/Cpp/2d_ecs_engine/libs/header/imgui/imgui_impl_sdl2.cpp" "CMakeFiles/main.dir/libs/header/imgui/imgui_impl_sdl2.cpp.obj" "gcc" "CMakeFiles/main.dir/libs/header/imgui/imgui_impl_sdl2.cpp.obj.d"
+  "C:/Users/Administrator/Projects/Cpp/2d_ecs_engine/libs/header/imgui/imgui_impl_sdlrenderer2.cpp" "CMakeFiles/main.dir/libs/header/imgui/imgui_impl_sdlrenderer2.cpp.obj" "gcc" "CMakeFiles/main.dir/libs/header/imgui/imgui_impl_sdlrenderer2.cpp.obj.d"
+  "C:/Users/Administrator/Projects/Cpp/2d_ecs_engine/libs/header/imgui/imgui_tables.cpp" "CMakeFiles/main.dir/libs/header/imgui/imgui_tables.cpp.obj" "gcc" "CMakeFiles/main.dir/libs/header/imgui/imgui_tables.cpp.obj.d"
+  "C:/Users/Administrator/Projects/Cpp/2d_ecs_engine/libs/header/imgui/imgui_widgets.cpp" "CMakeFiles/main.dir/libs/header/imgui/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/main.dir/libs/header/imgui/imgui_widgets.cpp.obj.d"
+  "C:/Users/Administrator/Projects/Cpp/2d_ecs_engine/src/Main.cpp" "CMakeFiles/main.dir/src/Main.cpp.obj" "gcc" "CMakeFiles/main.dir/src/Main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ make/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/libs/header/glm/detail/glm.cpp.obj"
+  "CMakeFiles/main.dir/libs/header/glm/detail/glm.cpp.obj.d"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui.cpp.obj"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui.cpp.obj.d"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui_demo.cpp.obj"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui_demo.cpp.obj.d"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui_draw.cpp.obj"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui_draw.cpp.obj.d"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui_impl_sdl2.cpp.obj"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui_impl_sdl2.cpp.obj.d"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui_impl_sdlrenderer2.cpp.obj"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui_impl_sdlrenderer2.cpp.obj.d"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui_tables.cpp.obj"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui_tables.cpp.obj.d"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui_widgets.cpp.obj"
+  "CMakeFiles/main.dir/libs/header/imgui/imgui_widgets.cpp.obj.d"
+  "CMakeFiles/main.dir/src/Main.cpp.obj"
+  "CMakeFiles/main.dir/src/Main.cpp.obj.d"
+  "libmain.dll.a"
+  "main.exe"
+  "main.exe.manifest"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
