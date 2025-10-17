2d905c72903d69d8ced09f24f74469d0f15b9642
toprakxx <179343804+toprakxx@users.noreply.github.com>
2025-10-18 02:27:44 +0300

    link system libraries & add c++20 flag

--- flake.nix
+++ flake.nix
@@ -11,27 +11,42 @@
       
       buildDeps = with pkgs; [
         # packages needed on runtime of the program
-        sdl2
-        lua
+        libcxx
+        SDL2
+        SDL2_image
+        SDL2_ttf
+        SDL2_mixer
+        lua5_4
       ];
       
       nativeDeps = with pkgs;[
         # packages needed to build the project
+        zig_0_14
       ];
       
       sharedEnv = {
         # environment for nix shell and build time
+        ZIG_GLOBAL_CACHE_DIR = "./.zig-cache";
+        C_INCLUDE_PATH="${pkgs.SDL2}/include/SDL2:${pkgs.SDL2_image}/include/SDL2:${pkgs.SDL2_ttf}/include/SDL2:${pkgs.SDL2_mixer}/include/SDL2:$C_INCLUDE_PATH";
+        LIBRARY_PATH="${pkgs.SDL2}/lib:${pkgs.SDL2_image}/lib:${pkgs.SDL2_ttf}/lib:${pkgs.SDL2_mixer}/lib:$LIBRARY_PATH";
       };
       
     in {
       packages.x86_64-linux.default = pkgs.stdenv.mkDerivation ({
-        pname = "2d-ecs-engine"; # change
+        pname = "2d-ecs-engine";
         version = "1.0.0";
         src = ./.;
         buildInputs = buildDeps;
         nativeBuildInputs = nativeDeps;
-        #buildPhase = '' '';
-        #installPhase = '' '';
+        buildPhase = ''
+
+          zig build
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          ls -a
+          cp zig-out/bin/* $out/bin/ 
+          '';
       } // sharedEnv);
 
       devShells.x86_64-linux.default = pkgs.mkShell ({
