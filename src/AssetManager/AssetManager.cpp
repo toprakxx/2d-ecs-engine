2424156442b61c24bc8828943a952e61f4a1ba01
toprakxx <179343804+toprakxx@users.noreply.github.com>
2025-10-18 02:25:47 +0300

    Add fallback definition for ASSETS_PATH

--- src/AssetManager/AssetManager.cpp
+++ src/AssetManager/AssetManager.cpp
@@ -7,6 +7,11 @@
 #include "SDL_surface.h"
 #include "SDL_ttf.h"
 
+#ifndef ASSETS_PATH
+#define ASSETS_PATH "./assets/"
+#endif
+
+
 AssetManager::AssetManager() {
 	Logger::Confirm("Asset Manager consturcted.");
 }
